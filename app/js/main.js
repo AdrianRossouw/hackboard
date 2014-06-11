e8777a4da9e93159ce9393d0d84289053cb97a61
Adrian Rossouw <adrian@daemon.co.za>
2014-06-11 18:34:54 +0200

    latest debug code

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 var _ = require('underscore');
 var Engine = require('famous/core/Engine');
 var View = require('famous/core/View');
+var Surface = require('famous/core/Surface');
+
 var Transform = require('famous/core/Transform');
 var StateModifier = require('famous/modifiers/StateModifier');
 var EventHandler      = require('famous/core/EventHandler');
@@ -19,7 +21,16 @@ var physicsEngine = new PhysicsEngine();
 var eventHandler = new EventHandler();
 eventHandler.on('post-render', _buildWalls);
 
-var boardSize = [3840, 2160];
+var boardSize = [384, 216];
+var boardBorder = [boardSize[0] + 300, boardSize[1] + 300];
+
+var area = new Surface({
+    size: boardBorder,
+    properties: {
+        backgroundColor: 'red'
+    }
+});
+
 
 var board = new Whiteboard({
     size:  boardSize,
@@ -32,22 +43,33 @@ board.body = new Rectangle({
 
 physicsEngine.addBody(board.body);
 
+board.state = new StateModifier({origin:[0.5,0.5]});
+
+board.on("click",function(){
+    console.log(arguments);
+   board.body.setVelocity([1,1,0]);
+});
 
 var draggable = new Draggable({});
 draggable.subscribe(board);
 
+draggable.on('update', function(e) {
+    console.log(e);
+});
 board.pipe(eventHandler);
 var modifier = new StateModifier({
     origin: [0.5, 0.5]
 });
 
 var node = mainContext.add(modifier);
-node.add(draggable).add(board);
+node.add(area);
+node.add(board.state).add(draggable).add(board);
 
 function _buildWalls(data) {
     var contextSize = mainContext.getSize();
     var surfaceSize = data.getSize();
 
+    surfaceSize = boardBorder;
     var walls = [];
     walls.push(_wall([1, 0, 0], contextSize[0], surfaceSize[0]));
     walls.push(_wall([-1, 0, 0], contextSize[0], surfaceSize[0]));
